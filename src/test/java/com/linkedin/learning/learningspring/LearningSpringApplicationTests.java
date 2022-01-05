07b029ea20951ab520085135d2bd2406be94e520
nikhilpawar43 <nikhilpawar43@ymail.com>
2022-01-05 16:11:38 +0530

    Initial project commit.

---
+++ src/test/java/com/linkedin/learning/learningspring/LearningSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linkedin.learning.learningspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
